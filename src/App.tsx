bb8c3df1dec07d47e8536436e094ab79f1427c20
kelvinko <kelvinkoko@gmail.com>
2022-07-02 00:25:06 +0900

    Update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 import * as React from "react";
 import { useState } from "react";
 import { hot } from "react-hot-loader/root";
+import styles from "./App.module.css";
 import CodeMirror from "./codemirror/CodeMirror";
 import { CodeMirrorStyle } from "./model/CodeMirrorStyle";
 import ColorSelector from "./ui/ColorSelector";
-
 const App = () => {
   const [cmStyle, setCmStyle] = useState<CodeMirrorStyle>({});
   return (
-    <>
-      <CodeMirror style={cmStyle} />
+    <div className={styles.container}>
+      <TopMenu />
+      <div className={styles.main}>
+        <LeftMenu />
+        <CodeMirror style={cmStyle} />
+      </div>
+    </div>
+  );
+};
+
+const TopMenu = () => {
+  return <div className={styles.topMenu}></div>;
+};
+
+const LeftMenu = (
+  cmStyle: CodeMirrorStyle,
+  setCmStyle: (style: CodeMirrorStyle) => void
+) => {
+  return (
+    <div className={styles.leftMenu}>
       <ColorSelector
         onChange={color => {
           setCmStyle({ ...cmStyle, editorBackgroundColor: color });
         }}
       />
-    </>
+    </div>
   );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,41 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const config = {
-  entry: [
-    'react-hot-loader/patch',
-    './src/index.tsx'
-  ],
+  entry: ["react-hot-loader/patch", "./src/index.tsx"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
+        use: ["style-loader", "css-loader"],
         exclude: /\.module\.css$/
       },
       {
         test: /\.ts(x)?$/,
-        loader: 'ts-loader',
+        loader: "ts-loader",
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
         use: [
-          'style-loader',
+          "style-loader",
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
               importLoaders: 1,
-              modules: true
+              modules: {
+                localIdentName: "[local]_[hash:base64:5]"
+              }
             }
           }
         ],
@@ -49,9 +45,9 @@ const config = {
         test: /\.png$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
-              mimetype: 'image/png'
+              mimetype: "image/png"
             }
           }
         ]
@@ -59,24 +55,23 @@ const config = {
     ]
   },
   devServer: {
-    'static': {
-      directory: './dist'
+    static: {
+      directory: "./dist"
     }
   },
   plugins: [
     new HtmlWebpackPlugin({
-      templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
-      filename: 'index.html',
+      templateContent: ({ htmlWebpackPlugin }) =>
+        '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
+        htmlWebpackPlugin.options.title +
+        '</title></head><body><div id="app"></div></body></html>',
+      filename: "index.html"
     })
   ],
   resolve: {
-    extensions: [
-      '.tsx',
-      '.ts',
-      '.js'
-    ],
+    extensions: [".tsx", ".ts", ".js"],
     alias: {
-      'react-dom': '@hot-loader/react-dom'
+      "react-dom": "@hot-loader/react-dom"
     }
   }
 };
