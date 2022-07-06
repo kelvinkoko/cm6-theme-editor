dcf0c9d1aca22a01269e33b5733efe894338bd18
kelvinko <kelvinkoko@gmail.com>
2022-07-07 00:22:05 +0900

    Extract to setStyleField

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { hot } from "react-hot-loader/root";
 import styles from "./App.module.css";
 import CodeMirror from "./codemirror/CodeMirror";
 import { CodeMirrorStyle } from "./model/CodeMirrorStyle";
+import { Color } from "./model/Color";
 import DefaultStyle from "./model/DefaultStyle";
 import { TEMPLATE_JS } from "./model/TemplateString";
 import SectionHeader from "./ui/SectionHeader";
@@ -41,20 +42,29 @@ const LeftMenu = (
         className={styles.styleItem}
         title={"Background"}
         initialColor={cmStyle.editorBackgroundColor}
-        setColor={color => {
-          setCmStyle({ ...cmStyle, editorBackgroundColor: color });
-        }}
+        setColor={setStyleField(cmStyle, setCmStyle, "editorBackgroundColor")}
       />
       <StyleItem
         className={styles.styleItem}
         title={"Text"}
         initialColor={cmStyle.editorColor}
-        setColor={color => {
-          setCmStyle({ ...cmStyle, editorColor: color });
-        }}
+        setColor={setStyleField(cmStyle, setCmStyle, "editorColor")}
       />
     </div>
   );
+};
+
+// Function that set a particular field of the CodeMirrorStyle object
+// and return a new object with the new value.
+const setStyleField = (
+  cmStyle: CodeMirrorStyle,
+  setCmStyle: (style: CodeMirrorStyle) => void,
+  field: keyof CodeMirrorStyle
+): ((color: Color) => void) => {
+  return (color: Color) => {
+    const newStyle: CodeMirrorStyle = { ...cmStyle, [field]: color };
+    setCmStyle(newStyle);
+  };
 };
 
 export default hot(App);
