edc6131b98580dbd3255810854219b166ab8b5d8
kelvinko <kelvinkoko@gmail.com>
2022-07-17 15:12:40 +0900

    Add cursor field

--- src/model/DefaultStyle.tsx
+++ src/model/DefaultStyle.tsx
@@ -19,6 +19,20 @@ const DefaultStyle: CodeMirrorStyle = {
           appendToTheme: color => {
             return { "&": { color: toHexString(color) } };
           }
+        },
+        {
+          name: "Cursor",
+          color: toColor("#000000"),
+          appendToTheme: color => {
+            return {
+              ".cm-content": {
+                caretColor: toHexString(color)
+              },
+              "&.cm-focused .cm-cursor": {
+                borderLeftColor: toHexString(color)
+              }
+            };
+          }
         }
       ]
     }
